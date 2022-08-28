7b613cac587f02323643015ed4ea755db6d277c4
Nirwash <nirwash@bk.ru>
2022-08-28 20:20:37 +0300

    refactoring

--- app/src/main/java/com/nirwashh/android/cleanarchitectureexample/MainActivity.kt
+++ app/src/main/java/com/nirwashh/android/cleanarchitectureexample/MainActivity.kt
@@ -2,20 +2,17 @@ package com.nirwashh.android.cleanarchitectureexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
 import com.nirwashh.android.cleanarchitectureexample.databinding.ActivityMainBinding
 import com.nirwashh.android.cleanarchitectureexample.domain.models.SaveUserNameParam
 import com.nirwashh.android.cleanarchitectureexample.domain.usecase.GetUserNameUseCase
 import com.nirwashh.android.cleanarchitectureexample.domain.usecase.SaveUserNameUseCase
-import com.nirwashh.android.cleanarchitectureexample.domain.usecase.ShowResultToastUseCase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private val getUserNameUseCase = GetUserNameUseCase()
     private val saveUserNameUseCase = SaveUserNameUseCase()
-    private val showResultToastUseCase = ShowResultToastUseCase()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +26,7 @@ class MainActivity : AppCompatActivity() {
             val text = binding.edEnterUserName.text.toString()
             val params = SaveUserNameParam(name = text)
             val result = saveUserNameUseCase.execute(param = params)
-            val resultText = showResultToastUseCase.execute(flag = result).text
-            binding.tvShowUserName.text = resultText
+            
         }
         buttonGetUserName.setOnClickListener {
             val text = getUserNameUseCase.execute().toString()
